f8792197a025d843f9ade63baf1445b78647e8fa
fvillarrealpadilla <f.villarreal.padilla@accenture.com>
2017-05-12 12:20:45 -0500

    add maximo

--- algoritmo.py
+++ algoritmo.py
@@ -4,11 +4,13 @@ global pesos;
 global prom_pesos;
 global prom_cap;
 global pre_mochila;
+global disponibles;
 pre_mochila=[]
 prom_cap=[]
 datos=[]
 cap=0
 prom_pesos=[]
+disponibles=[]
 def prom(lista):
     promedio=0.0
     aux=0.0
@@ -26,7 +28,7 @@ def suma():
             aux+=datos[x][y]
         sumas.append(aux)
         aux=0
-    print sumas
+    #print sumas
 def leerDatos():
     line=0;
     aux=[]
@@ -35,21 +37,21 @@ def leerDatos():
     archivo = open("1000_25/1000_25_1.txt", "r")
     for linea in archivo.readlines():
         for x in linea:
-        	if x!= " " and x!="\n":
+            if x!= " " and x!="\n":
                     if line>=1:
                         suma+=x;
                     else:
                         line+=1
-
-        	else:
+                        
+            else:
                     if line>=1 and (len(suma)>0):
                         aux.append(int(suma))
                         suma=""
         if(len(aux)>0):
             if len(datos)<1000:
-            	#print aux
-            	datos.append(aux)
-            	aux=[]
+                #print aux
+                datos.append(aux)
+                aux=[]
             else:
                 if cap==0:
                     cap=aux[0]
@@ -66,25 +68,38 @@ def rellenar():
             else:
                 break
     #print len(datos[500])
-
-
+def validar(objeto):
+    if objeto+100<=cap:
+        return True
+    else:
+        return False
+def mejores(lista):
+    print disponibles
+    i=0
+    aux=[]
+    valido=True
+    for x in range(10):
+        while valido:
+            i=lista.index(max(lista))
+            if disponibles[i]==0:
+                lista[i]=0
+                aux.append(i)
+                valido=False
+    print aux
+        
 def pre(lista):
+    prePeso=0
+    sinCambios=0
     disponibles=[0]*len(datos)
     prom_pesos=prom(lista)
     for x in range(len(datos)-1):
-        #print datos[x]
         prom_cap.append(prom(datos[x+1]))
-        if(prom_cap[x]!=0):
-            disponibles[x]=1
-    #print prom_cap
-    #print len(prom_cap)
-    #print disponibles
-    #while cap<:
-     #   pass
-
+    mejores(lista)
+    #while prePeso<cap and sinCambios<10:
+     #   for x in range():
+      #      pass
 
 pesos=leerDatos()
 rellenar()
 suma()
 pre(pesos)
-print "hola prros de git"
